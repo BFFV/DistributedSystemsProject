a928ca8c1d63d2a9d05d9729046ac5a4666c7c8c
BFFV <bffarias@uc.cl>
2021-09-04 13:43:53 -0400

    :card_file_box: Setup for db.

--- chat_app/__init__.py
+++ chat_app/__init__.py
@@ -7,23 +7,29 @@ from flask_migrate import Migrate
 migrate = Migrate()
 socketio = SocketIO()
 
+# Env vars
+db_user = 'postgres'
+db_password = 'postgres'
+db_url = 'localhost:5432'
+db_name = 'chat_db'
 
+
+# Create app
 def create_app():
-    # create and configure the app
+    # Create and configure the app
     app = Flask(__name__)
 
     # Database configuration
-    database_name = 'chat_distribuidos'
     default_database_path = "postgresql://{}:{}@{}/{}" \
-        .format('postgres', 'postgres', 'localhost:5432', database_name)
-    app.config["SQLALCHEMY_DATABASE_URI"] = default_database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        .format(db_user, db_password, db_url, db_name)
+    app.config['SQLALCHEMY_DATABASE_URI'] = default_database_path
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    # App configuration
     socketio.init_app(app)
     from .models import db, Message
     db.init_app(app)
     migrate.init_app(app, db)
-
     CORS(app)
 
     @app.route('/')
@@ -37,12 +43,8 @@ def create_app():
     @socketio.on('message')
     def handle_message(json):
         print('received message: ' + str(json))
-
         message = Message(json["message"], json["user_name"])
         Message.insert(message)
-
         socketio.emit('my response', json)
 
     return app
-
-
