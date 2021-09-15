e73003ce701008a6c451a2caa27b5714eee67e63
fguinez <fhguinez@uc.cl>
2021-09-15 11:57:02 -0300

    🧼 Improve parameters on server main wrapper

--- chat_app/main.py
+++ chat_app/main.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     # Extrae el path ejecutado sin 'main.py'
     path = '/'.join(sys.argv[0].split('/')[:-1] + [''])
     # Ejecuta '__init_.py' ubicado en el mismo path que 'main.py'
-    os.system(f'python3 {path}__init__.py {sys.argv[-1]}')
+    os.system(f'python3 {path}__init__.py {" ".join(sys.argv[1:])}')
