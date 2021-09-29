b33f751190f54300d0a52d544b418b4a798b1780
Christian-Klempau <christian.klempau@uc.cl>
2021-09-29 11:06:34 -0300

    Changed logic to use OOP and modules.

---
+++ server/server.py
@@ -0,0 +1,49 @@
+from random import choice
+from user import User
+
+
+class Server:
+    def __init__(self) -> None:
+        # Client counter
+        self.N_CLIENTS_REQUIRED = 2
+        self.n_clients = 0
+
+        # Users
+        self.users = dict()  # Key: socket ID, Value: Username
+        self.user_data = dict()  # Key: Username, Value: IP/Port
+
+        # Usernames set (for quickly checking existence)
+        self.usernames = set()
+
+        # Messages
+        self.messages = []
+
+    def set_params(self, n_clients=2):
+        self.N_CLIENTS_REQUIRED = n_clients
+        self.n_clients = 0
+        self.users = dict()
+        self.user_data = dict()
+        self.usernames = set()
+        self.messages = []
+        print(f'Server initialized (N = {self.N_CLIENTS_REQUIRED})')
+
+    def __getitem__(self, socket_id):
+        """ Al acceder a la clase con llave [socket_id], retorna el nombre de usuario. """
+        try:
+            return self.users[socket_id].username
+        except KeyError:
+            print(f"El cliente con {socket_id=} no existe!")
+
+    def build_user(self, username, socket_id, *args):
+        # args == username, socket_id, ip, port, node_id
+        self.n_clients += 1
+        self.users[socket_id] = User(username, socket_id, *args)
+        self.usernames.add(username)
+
+    def get_user_connections(self, username):
+        for user in self.users.values():
+            if user.username == username:
+                return user.get_connections()
+
+    def find_future_server(self):
+        return choice(self.users)
---
+++ server/user.py
@@ -0,0 +1,13 @@
+class User:
+    def __init__(self, username, socket_id, ip, port, node_id):
+        # Identification
+        self.username = username
+        self.socket_id = socket_id
+
+        # Connections
+        self.ip = ip
+        self.port = port
+        self.node_id = node_id
+
+    def get_connections(self):
+        return (self.ip, self.port, self.node_id)
