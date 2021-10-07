9b80fe779a425e239c425f30cd40063b1957ebd8
fguinez <fhguinez@uc.cl>
2021-10-07 20:04:45 -0300

    ✨ Now modules works from all paths

--- server/import_fixer.py
+++ server/import_fixer.py
@@ -2,6 +2,13 @@ import os
 import sys
 
 
+
+
 def fix_imports():
-    new_path = os.path.abspath(os.path.join("libs"))
+    this_file_path = os.path.join(os.getcwd(), os_py_path())
+    root_path = '/'.join(this_file_path.rstrip('/').split('/')[:-1])
+    new_path = os.path.join(root_path, 'libs')
     sys.path.append(new_path)
+
+def os_py_path():
+    return '/'.join(sys.argv[0].split('/')[:-1])
